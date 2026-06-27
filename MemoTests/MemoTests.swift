7e65cedcbc867119b5e81db3840dc72c541edfbe
Bhara Alfhaniawan <bharalfhani@gmail.com>
2026-06-27 21:14:09 +0700

    Initial Commit

---
+++ MemoTests/MemoTests.swift
@@ -0,0 +1,19 @@
+//
+//  MemoTests.swift
+//  MemoTests
+//
+//  Created by Bhara Alfhaniawan on 27/06/26.
+//
+
+import Testing
+@testable import Memo
+
+struct MemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
